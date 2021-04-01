187a1bfe2caa5d092ae6f27a0dec2773ba5110b8
Sarman7105 <sarman7105@gmail.com>
2021-04-01 22:09:10 +0600

    manage product before delete implemented

--- src/Components/AdminMenu/AdminMenu.js
+++ src/Components/AdminMenu/AdminMenu.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const AdminMenu = () => {
+const AdminMenu = ({setIsAddingBook}) => {
+    const handleAdd = () => {
+        setIsAddingBook(true);
+    }
+    const handleManage = () => {
+        setIsAddingBook(false);
+    }
     return (
         <div>
-            <h3>Admin menu</h3>
+            <h2 className="text-center">Book Shop</h2>
+            <h5 className="text-center" onClick={handleManage}>Manage Book</h5>
+            <h5 className="text-center" onClick={handleAdd}>Add Book</h5>
+            
         </div>
     );
 };
--- src/Components/AdminPanel/AdminPanel.js
+++ src/Components/AdminPanel/AdminPanel.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AddBook from '../AddBook/AddBook';
 import AdminMenu from '../AdminMenu/AdminMenu';
-import './AdminPanel.css'
+import ManageProduct from '../ManageProduct/ManageProduct';
+import './AdminPanel.css';
 
 const AdminPanel = () => {
-    return (
-        <div className="row panel-container">
-            <div className="col-md-2 admin-menu">
-                <div >
-                    <AdminMenu/>
-                </div>
-            </div>
-            <div className="col-md-10 admin-data">
-                <div>
-                    <AddBook></AddBook>
-                </div>
-            </div>
-        </div>
-    );
+	const [ isAddingBook, setIsAddingBook ] = useState(true);
+	const [ books, setBooks ] = useState([]);
+	useEffect(() => {
+		fetch('http://localhost:5055/books').then((res) => res.json()).then((data) => setBooks(data));
+	}, []);
+	return (
+		<div className="row panel-container">
+			<div className="col-md-2 admin-menu">
+				<div>
+					<AdminMenu setIsAddingBook={setIsAddingBook} />
+				</div>
+			</div>
+			<div className="col-md-10 admin-data">
+				<div>
+					{isAddingBook ? (
+						<AddBook />
+					) : (
+						<table class="table table-borderless">
+							<thead>
+								<tr>
+									<th scope="col">Book Name</th>
+									<th scope="col">Author Name</th>
+									<th scope="col">Price</th>
+									<th scope="col">Action</th>
+								</tr>
+                            </thead>
+                            <tbody>
+                                    {
+                                        books.map(book=><ManageProduct book={book}></ManageProduct>)
+                                    }
+                            </tbody>
+						</table>
+						
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AdminPanel;
